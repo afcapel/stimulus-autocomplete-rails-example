e09c73310f9302588370f54fd10f598fc2c4b676
Alberto Fernandez-Capel <alberto@hey.com>
2021-10-10 12:04:35 +0100

    Fix scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,21 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+puts "Seeding database"
+
+Bird.find_or_create_by!(name: "Mockingbird") do |b|
+  b.description = "New World passerine birds from the family Mimidae."
+end
+
+Bird.find_or_create_by!(name: "Bluebird") do |b|
+  b.description = "Medium-sized, mostly insectivorous or omnivorous birds in the order of Passerines"
+end
+
+Bird.find_or_create_by!(name: "Blackbird") do |b|
+  b.description = "A species of true thrush"
+end
+
+puts "#{Bird.count} birds in the database"
